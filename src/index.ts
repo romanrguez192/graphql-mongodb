918fa290465d90bd7966e0c6e667b9fdcc00897b
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-11-14 14:18:52 -0400

    Apollo Server

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
+
+import { typeDefs, resolvers } from "./schema";
+
+const startApolloServer = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+};
+
+startApolloServer();
